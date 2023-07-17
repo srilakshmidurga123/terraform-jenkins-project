ff69f5b721506f326404ee5ceaead6a21d9ee60c
srilakshmidurga123 <134672922+srilakshmidurga123@users.noreply.github.com>
2023-07-17 18:48:52 +0530

    Update Backend.tf

--- Backend.tf
+++ Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-dev-tf-state-bucket-11"
-    key = "master"
+    bucket = "my-dev-tf-state-bucket-33"
+    key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamo-table"
   }
